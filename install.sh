a6904ecd54ad630dbd1b46ef7ef8178d4d910897
Mark Thurston <mark@mdvthu.com>
2022-12-17 18:10:51 +0000

    Check if config dir exists

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # Installation script
 
-# Copy over the configuration files
-VIMRC_DIR="${HOME}/.config/nvim/"
-cp -r nvim/ "$VIMRC_DIR"
+CONF_DIR="${HOME}/.config"
+VIMRC_DIR="${CONF_DIR}/nvim/"
 
+if [ -d $VIMRC_DIR ]; then
+  echo $VIMRC_DIR exists. Exiting.
+  exit 1
+fi
+
+# Copy over the configuration files
+cp -r nvim "$CONF_DIR"
 # Headless install of the plugins
 nvim -c 'PluginInstall' -c 'qa'
