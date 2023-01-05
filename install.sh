9160855d8687a667db795ac4b16e3a463542cdc8
Mark Thurston <mark@mdvthu.com>
2023-01-05 20:48:23 +0000

    Include Vundle installation in the install script

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ if [ -d $VIMRC_DIR ]; then
   exit 1
 fi
 
+# Install Vundle
+git clone \
+  https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+
 # Copy over the configuration files
 cp -r nvim "$CONF_DIR"
 # Headless install of the plugins
